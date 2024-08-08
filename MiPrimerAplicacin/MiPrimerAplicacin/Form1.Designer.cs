60fea05e8f1ac933d2cd793788a0fbaf52838f78
Xuxa <judith.carolina.diaz@gmail.com>
2024-08-08 20:36:20 -0300

    camnie, nombre y color de fondo

--- MiPrimerAplicacin/MiPrimerAplicacin/Form1.Designer.cs
+++ MiPrimerAplicacin/MiPrimerAplicacin/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MiPrimerAplicacin
 {
-    partial class Form1
+    partial class cambieNombre
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // cambieNombre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "cambieNombre";
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MiPrimerAplicacin/MiPrimerAplicacin/Form1.cs
+++ MiPrimerAplicacin/MiPrimerAplicacin/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace MiPrimerAplicacin
 {
-    public partial class Form1 : Form
+    public partial class cambieNombre : Form
     {
-        public Form1()
+        public cambieNombre()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
