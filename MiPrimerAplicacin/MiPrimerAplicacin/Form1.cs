c024bec063fe0e4e471b7244603ef3def13a881a
Xuxa <judith.carolina.diaz@gmail.com>
2024-08-09 13:54:12 -0300

    Update Form1.cs

--- MiPrimerAplicacin/MiPrimerAplicacin/Form1.cs
+++ MiPrimerAplicacin/MiPrimerAplicacin/Form1.cs
@@ -15,10 +15,20 @@ namespace MiPrimerAplicacin
         public cambieNombre()
         {
             InitializeComponent();
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+         
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MessageBox.Show(" Bienvenidos a c#");
+
+        }
+  
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show(" chau, nos vamos de C#...." );
 
         }
     }
